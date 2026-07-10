ff7ca7d5e7b6dd2bdc692f0000c9a87ba44d0671
khanquadir19 <khanquadir19@gmail.com>
2026-07-10 06:55:40 +0000

    chore: remove unused imports (padding, Card) from JournalCard.kt

--- app/src/main/java/fieldmind/research/app/features/field/presentation/components/JournalCard.kt
+++ app/src/main/java/fieldmind/research/app/features/field/presentation/components/JournalCard.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
